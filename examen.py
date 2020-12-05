17f47bf7ef6c4a83586ce2b20b742d777757a7c6
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2020-12-05 14:37:14 -0600

    prueba de la lineal

--- examen.py
+++ examen.py
@@ -25,3 +25,36 @@ print("prueba generacion individuo:")
 ind = generarIndividuoLineal(LIM_M, lim_b)
 print(ind[0]) #m
 print(ind[1]) #b
+
+#iteracion 1
+	#generar poblacion con 100 individuos, calcularle su Z 
+	#ver en esa poblacion el de Mayor Z y guardarlo
+
+print("\npoblacion 1")
+poblacion = [] 
+mejorZ = -999999
+mejorM = 0
+mejorB = 0
+idVector = 0
+pob = 0
+for j in range(1000):
+	mejorZPob = -999999
+	mejorMPob = 0
+	mejorBPob = 0
+	idVectorPob = 0
+	for i in range(1000):
+		propuestas = generarIndividuoLineal(LIM_M, lim_b)
+		z = Z_lineal(puntos, propuestas[0], propuestas[1])
+		if(z > mejorZPob):
+			mejorZPob = z
+			mejorMPob = propuestas[0]
+			mejorBPob = propuestas[1]
+			idVectorPob = i
+	print("Mejor vector de poblacion ",j,":", idVectorPob, mejorMPob, mejorBPob, mejorZPob)
+	if(mejorZPob > mejorZ):
+		mejorZ = mejorZPob
+		mejorM = mejorMPob
+		mejorB = mejorBPob
+		idVector = idVectorPob
+		pob = j
+print("MEJOR poblacoin",pob,", vector", idVector,": m=",mejorM, ", b=",mejorB, "Z=", mejorZ)
