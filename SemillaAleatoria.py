a6a71c121cc149d899226c1804d309ddac88cc77
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2020-12-05 13:14:55 -0600

    funcion objetivo de la gaussiana

--- SemillaAleatoria.py
+++ SemillaAleatoria.py
@@ -1,3 +1,5 @@
+import math
+
 def restriccion(value, lim, tipo):
 	cumple = False
 	if tipo == 0: #<=
@@ -8,9 +10,16 @@ def restriccion(value, lim, tipo):
 			cumple = True
 	return cumple
 
-def Z(puntos, m, b):
+def Z_lineal(puntos, m, b):
 	z_value = 0 
 	for (x,y) in puntos:
 		z_value += abs(m*x + b - y)
+	z_value *= -1 
+	return z_value
+
+def Z_gaussiana(puntos, k, m):
+	z_value = 0 
+	for (x,y) in puntos:
+		z_value += abs(math.exp(-k*((x-m)**2)))
 	z_value *= -1 
 	return z_value
--- examen.py
+++ examen.py
@@ -5,8 +5,14 @@ lim_m = 100
 lim_b = 132.5
 r = 80
 print(restriccion(r, lim_m, 0))
-print("prueba Z")
+
+print("prueba Z lineal")
 puntos = [[1, 0.42], [3, 0.75],[3.5, 1],[4, 0.42]]
-m = -82.67716535
-b = 54.55882353
-print(Z(puntos, m, b))
+m = .1863
+b = .2369
+print(Z_lineal(puntos, m, b))
+
+print("prueba Z gaussiana")
+k = -0.1487
+m = 3.5
+print(Z_gaussiana(puntos, k, m))
