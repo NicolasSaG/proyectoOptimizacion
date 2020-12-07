aba0e65e67df577f24c6b53e58e475bcae991a9f
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2020-12-06 19:14:08 -0600

    se agrega la configuracion de num de iteraciones y de tam de poblacion

--- examen.py
+++ examen.py
@@ -1,23 +1,20 @@
 from SemillaAleatoria import *
 import ManejoArchivo as archivo
+
 LIM_M = 100 #lim de la pendiente
 LIM_TIEMPO = 30 #lim de ejecucion en segundos
 
-#Valores prueba
-#puntos1 = [[1, 0.42], [3, 0.75],[3.5, 1],[4, 0.42]]
-#puntos2 = [[.2, 0.1], [.5, 0.83],[.9,.4]]
-#puntos3 = [[15,.3], [25,.45],[30,.78], [34, 1], [40,0.85]]
-
-#almanecar mejores globales
-mejorZ = 10000
-mejorM = 0
-mejorB = 0
-idVector = 0
-pob = 0
 #Ingreso de puntos#
 print("Porfavor ingrese una ruta valida para obtener los puntos a usar :)")
 ruta=input()
 puntos = archivo.LecturaArchivo(ruta).tolist()
+
+print("Numero de iteraciones (1 a 100):")
+num_iteraciones = int(input())
+
+print("Tamano de la poblacion (se recomienda 1000):")
+tam_poblacion = int(input())
+
 
 
 lim_b = calcular_bLineal(puntos)
@@ -26,15 +23,21 @@ print("limites de b: [",-lim_b,",",lim_b,"]")
 #generacion de semilla
 random.seed(generarSemillaAleatoria()) 
 
+#almanecar mejores globales
+mejorZ = 10000
+mejorM = 0
+mejorB = 0
+idVector = 0
+pob = 0
 inicioTiempo = obtenerTiempo()
 finTiempo = 0
-for j in range(100):
+for j in range(num_iteraciones):
 	#mejores de cada poblacion
 	mejorZPob = 100000
 	mejorMPob = 0
 	mejorBPob = 0
 	idVectorPob = 0
-	for i in range(1000):
+	for i in range(tam_poblacion):
 		propuestas = generarIndividuoLineal(LIM_M, lim_b) #generacion de individuo
 		z = Z_lineal(puntos, propuestas[0], propuestas[1]) #calculo de f.o. con los valores del individuo
 		if(z < mejorZPob): #comparacion con el mejor actual de la poblacion
@@ -44,25 +47,25 @@ for j in range(100):
 			idVectorPob = i
 		#terminar si se pasa de 30 segundos
 		finTiempo = obtenerTiempo()
-		if(finTiempo - inicioTiempo > 30):
+		if(finTiempo - inicioTiempo > LIM_TIEMPO):
 			print("Me quede en la poblacion: {}  individuo: {}".format(j,i))
 			break
-	#print("Mejor vector de poblacion ",j,":", idVectorPob, mejorMPob, mejorBPob, mejorZPob)
+	print("El mejor de poblacion {}, vector {}:, m={}, b={}, Z={}".format(j, idVectorPob, mejorMPob, mejorBPob, mejorZPob))
 	if(mejorZPob < mejorZ): #comparacion con el mejor actual de la iteraciones
 		mejorZ = mejorZPob
 		mejorM = mejorMPob
 		mejorB = mejorBPob
 		idVector = idVectorPob
 		pob = j
-	if(finTiempo - inicioTiempo > 30):
+	if(finTiempo - inicioTiempo > LIM_TIEMPO):
 		print("Me quede en la poblacion: {}  individuo: {}".format(j,i))
 		break	
+
 print("tiempo de ejecucion: ", finTiempo - inicioTiempo, "s")
-print("MEJOR poblacion {}, vector {}:, m={}, b={}, Z={}".format(pob, idVector,mejorM,mejorB,mejorZ))
+print("MEJOR GLOBAL poblacion {}, vector {}:, m={}, b={}, Z={}".format(pob, idVector,mejorM,mejorB,mejorZ))
+
 #Funcion gaussiana
-print("Limite b",lim_b)
 print("Funcion gaussiana: ")
-poblacion = [] 
 mejorZ = 10000
 mejorM=Mayor(puntos)
 mejorK = 0
@@ -70,12 +73,12 @@ idVector = 0
 pob = 0
 infe=0
 supe=5
-for j in range(1000):
+for j in range(num_iteraciones):
 	mejorZPob = 10000
 	mejorKPob = 0
 	idVectorPob = 0
 	rango=1
-	for i in range(1000):
+	for i in range(tam_poblacion):
 		propuestas = generarIndividuoGauss(infe,supe)
 		z = Z_gaussiana(puntos, propuestas, mejorM)
 		if(z < mejorZPob):
@@ -85,7 +88,7 @@ for j in range(1000):
 			infe=mejorKPob-rango
 			idVectorPob = i
 		rango=rango/10
-	#print("Mejor vector de poblacion ",j,":", idVectorPob, -mejorKPob, bbmejorZPob)
+	print("El mejor de poblacion {}, vector {}:, m={} k={} Z={}".format(j, idVectorPob,mejorM, mejorKPob, mejorZPob))
 	if(mejorZPob < mejorZ):
 		mejorZ = mejorZPob
 		mejorK = mejorKPob
